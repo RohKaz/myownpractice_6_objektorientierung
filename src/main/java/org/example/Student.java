ea543df1b3a9ec07137e4f3b2fa114df03e46d58
RohKaz <rohula-kazimi@hotmail.com>
2023-02-28 19:53:06 +0100

    add getter and setter

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -2,8 +2,32 @@ package org.example;
 
 public class Student {
 
-    String id;
-    String name;
-    String subject;
+    private String id;
+    private String name;
+    private String subject;
 
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 }
