939f046270793e2676b5c2d7df233b4a1de51e51
RohKaz <rohula-kazimi@hotmail.com>
2023-02-28 20:13:32 +0100

    Basis Objekt-Methoden Aufgabe

--- src/main/java/org/example/model/Student.java
+++ src/main/java/org/example/model/Student.java
@@ -1,5 +1,7 @@
 package org.example.model;
 
+import java.util.Objects;
+
 public class Student {
 
     private String id;
@@ -44,5 +46,18 @@ public class Student {
                 ", name='" + name + '\'' +
                 ", subject='" + subject + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id.equals(student.id) && name.equals(student.name) && subject.equals(student.subject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, subject);
     }
 }
